c6a67c6062cdafc2598dde0ec2bfb5010e886801
Sam Tholiya <tholiya.sam@outlook.in>
2022-09-19 22:13:34 +0200

    script.py updated engine code

--- src/script.py
+++ src/script.py
@@ -1,7 +1,27 @@
 #!/usr/bin/python3
+import os
 from bitbucket_pipes_toolkit import Pipe
-from bitbucket_pipes_toolkit import ArrayVariable
+import yaml
+
 
 pipe = Pipe(pipe_metadata_file="./config.yaml")
 
-pipe.variables
+command_name = pipe.get_variable("command_name")
+
+with open(os.path.join("./","plugins", command_name + ".yaml"), "r") as stream:
+    try:
+        command_schema = yaml.safe_load(stream)
+        command = command_schema["command_prefix"]
+        for var in pipe.variables:
+            if var == "command_name":
+                continue
+            if var in command_schema["variable_mapping"]:
+                value = pipe.get_variable(var)
+                command = command + " " + command_schema["variable_mapping"][var] + "=" + "\"" + str(pipe.get_variable(var)) + "\""
+        print("Executing:", command)
+        exit_code = os.system(command)
+        if exit_code:
+            pipe.fail("command exited unsuccessfully", True)
+        pipe.success("Succesfully executed")
+    except yaml.YAMLError as exc:
+        print(exc)
