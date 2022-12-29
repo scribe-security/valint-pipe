64b3d448c29ed0a188e0872b7cf469f1f3956ef2
houdini91 <mdstrauss91@gmail.com>
2022-12-29 11:39:54 +0200

    test

--- src/script.py
+++ src/script.py
@@ -9,9 +9,12 @@ pipe = Pipe(pipe_metadata_file="/src/config.yaml")
 command_name = pipe.get_variable("COMMAND_NAME")
 target = pipe.get_variable("TARGET")
 
+print("########### 1", command_name)
+print("########### 2", target)
 if command_name == "version":
     command_schema = yaml.safe_load(stream)
     command = command_schema["command_prefix"]
+    print("########### 3", command)
     exit_code = os.system(command + "--version")
     if exit_code:
         pipe.fail("command exited unsuccessfully", True)
@@ -21,8 +24,10 @@ with open(os.path.join("/src/","plugins", command_name + ".yaml"), "r") as strea
     try:
         command_schema = yaml.safe_load(stream)
         command = command_schema["command_prefix"]
+        print("########### 4", command, command_schema)
         if  target is not None and target != "":
             command = command + " " + target
+            print("########### 5", command)
 
         for var in pipe.variables:
             if var == "COMMAND_NAME" or var == "TARGET":
@@ -30,17 +35,19 @@ with open(os.path.join("/src/","plugins", command_name + ".yaml"), "r") as strea
             if var in command_schema["variable_mapping"]:
                 value = pipe.get_variable(var)
                 command = command + " " + command_schema["variable_mapping"][var] + "=" + "\"" + str(pipe.get_variable(var)) + "\""
+                print("########### 6", command)
            
 
-        logger.info("> echo $(cd " + os.getenv('BITBUCKET_WORKSPACE') + " && " + command + " )")
         if os.getenv('BITBUCKET_WORKSPACE') != None:
-            print("############# 7", os.getenv('BITBUCKET_WORKSPACE'))
-            print("############# 8", command)
-
-            exit_code = os.system("echo $(cd " + os.getenv('BITBUCKET_WORKSPACE') + "/.. && " + command + " )")
+            print("########### 7", command)
+            print("########### 7", os.getenv('BITBUCKET_WORKSPACE'))
+            runCommand = "echo $(cd " + os.getenv('BITBUCKET_WORKSPACE') + "/.. && " + command + " )"
         else:
-            exit_code = os.system("echo $(" + command + ")")
-        
+            runCommand = "echo $(" + command + ")"
+
+
+        logger.info("> " + runCommand)
+        exit_code = os.system(runCommand)
         if exit_code:
             pipe.fail("command exited unsuccessfully", True)
         pipe.success("Succesfully executed")
