a5f14b31cc3369abd21cd6df8305e4fa124eb08d
houdini91 <mdstrauss91@gmail.com>
2022-12-29 13:42:57 +0200

    update

--- bump-version.sh
+++ bump-version.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-# Bump versions using semversioner
-
 set -ex
 
 previous_version=$(semversioner current-version)
 v_previous_version=v$previous_version
-# semversioner release
 
 env
 
@@ -17,17 +14,6 @@ then
 else
     new_version=$(semversioner next-version)
 fi
-
-# echo "Generating CHANGELOG.md file..."
-# semversioner changelog > CHANGELOG.md
-
-# # Use new version in the README.md examples
-# echo "Replace version '$previous_version' to '$new_version' in README.md ..."
-# sed -i "s/$previous_version/$new_version/g" README.md
-
-# # Use new version in the pipe.yml metadata file
-# echo "Replace version '$previous_version' to '$new_version' in pipe.yml ..."
-# sed -i "s/$previous_version/$new_version/g" setup.py
 
 echo "Replace version '$previous_version' to '$new_version' in Dockerfile ..."
 sed -i "s/$previous_version/$new_version/g" Dockerfile
