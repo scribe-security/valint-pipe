45e25d5102d919133e1a68928413081901d9792d
houdini91 <mdstrauss91@gmail.com>
2025-05-25 13:22:42 +0300

    update bump version

--- bump-version.sh
+++ bump-version.sh
@@ -5,16 +5,15 @@ set -ex
 previous_version=$(semversioner current-version)
 v_previous_version=v$previous_version
 
-if [ "$ENV" == "dev" ]
-then
+if [ "$ENV" == "dev" ]; then
   previous_version=$v_previous_version
   new_version="dev-latest"
 else
-    new_version=$(semversioner next-version)
+  new_version=$(semversioner next-version)
 fi
 
-echo "Replace version '$previous_version' to '$new_version' in Dockerfile ..."
-sed -i "s/$previous_version/$new_version/g" Dockerfile
+# Replace any version in the FROM line with the new version
+echo "Replace version in Dockerfile with '$new_version'..."
+sed -i "s|FROM scribesecuriy.jfrog.io/scribe-docker-public-local/valint:v[^\ ]*|FROM scribesecuriy.jfrog.io/scribe-docker-public-local/valint:$new_version|g" Dockerfile
+
 cat Dockerfile
-
-
